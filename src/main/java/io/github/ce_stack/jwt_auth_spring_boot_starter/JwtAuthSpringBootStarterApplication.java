dc30037eb1254579ceff89e4351a4df555788a59
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-09-21 09:11:31 +0300

    Initial commit

---
+++ src/main/java/io/github/ce_stack/jwt_auth_spring_boot_starter/JwtAuthSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.ce_stack.jwt_auth_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ce_stack/jwt_auth_spring_boot_starter/JwtAuthSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ce_stack.jwt_auth_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
