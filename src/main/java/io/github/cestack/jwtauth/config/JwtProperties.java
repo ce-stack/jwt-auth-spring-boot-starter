4ede0f396c5774e7229efe864ec6b0a6d53c7e7b
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-09-22 10:56:14 +0300

    fix

--- src/main/java/io/github/cestack/jwtauth/config/JwtProperties.java
+++ src/main/java/io/github/cestack/jwtauth/config/JwtProperties.java
@@ -3,7 +3,7 @@ package io.github.cestack.jwtauth.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.time.Duration;
-
+@ConfigurationProperties(prefix = "jwt-auth")
 public class JwtProperties {
 
 
